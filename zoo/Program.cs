ba57b801f723b378d2144aa14a731720100512ab
Goolos <tylenik@gmail.com>
2017-05-03 11:43:44 +0500

    add

---
+++ zoo/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace zoo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = File.ReadAllText(@"C:\Users\student\Desktop\text2.txt");
+            Console.WriteLine("Текст: " + text);
+            string[] date = text.Split(new char[] { '.' });
+            string[] punkt = null;
+
+            for (int i = 0; i < date.Length - 1; i++)
+            {
+                string newDate = date[i];
+                punkt = newDate.Split(new char[] { ';' });
+
+                string znachenie = punkt[0];
+                string newZnachenie = znachenie.ToLower();
+                Console.WriteLine(newZnachenie);
+                if (newZnachenie == "уссурийский тигр")
+                {
+                    Console.WriteLine("Название животного: {0}\r\nколичество вида: {1}\r\nадрес зоопарка: {2}\r\nобщее количество животных(в тоннах): {3}\r\nколичсетво работников: {4}\r\n", punkt[0], punkt[1], punkt[2], punkt[3], punkt[4]);
+                }
+            }
+        }
+    }
+}
