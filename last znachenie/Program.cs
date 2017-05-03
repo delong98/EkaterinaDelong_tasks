fb677f4ac61d7a59c923acdf80c4ea2de89c59bc
delong <delong.98@mail.ru>
2017-05-03 16:07:51 +0500

    add new console

---
+++ last znachenie/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace last_znachenie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = File.ReadAllText(@"C:\Users\Екатерина\Desktop\text3.txt");
+            Console.WriteLine(text);
+            string[] date = text.Split(new char[] { ',' });
+            int a = date.Length;
+            Console.WriteLine(date[a-1]);
+
+            Console.ReadKey();
+        }
+    }
+}
