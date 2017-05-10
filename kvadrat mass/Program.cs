24b650f6801cb3208d8080dc84a7a98045d6adea
delong <delong.98@mail.ru>
2017-05-10 14:10:37 +0500

    add

---
+++ kvadrat mass/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kvadrat_mass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random gen = new Random();
+            Random a = new Random();
+            int n = gen.Next(2, 6);
+            int[,] mas = new int[n, n];
+
+            for (int i = 0; i < mas.GetLength(0); i++)
+            {
+                for (int j = 0; j < mas.GetLength(1); j++)
+                {
+                    mas[i, j] = gen.Next(0, 10);
+                    Console.Write(mas[i, j] + "\t");
+                }
+                Console.WriteLine();
+            }
+
+            int sum = 0;
+            for (int i = 0; i < mas.GetLength(0); i++)
+            {
+                sum += mas[i, n - 1];
+            }
+            Console.WriteLine("сумма эл-ов последнего столбца: " + sum);
+            
+            Console.ReadKey();
+        }
+    }
+}
