206d869145feb20b2418b18851122f5bad330ad0
delong <delong.98@mail.ru>
2017-05-21 17:06:34 +0500

    add

---
+++ dictionary words/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dictionary_words
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = System.IO.File.ReadAllText(@"C:\Users\Екатерина\Desktop\text5.txt");
+            string[] newText = text.Split(' ', '-', '.', ',','(', ')', '"');
+
+            string a = null;
+
+            for (int i = 0; i <newText.Length; i++)
+            {
+                a += newText[i]+" ";
+            }
+            Console.WriteLine(a);
+
+
+
+            var dictionary = new Dictionary<string, int>();
+
+            for (int i = 0; i < newText.Length; i++)
+            {
+                if (newText[i] != "") 
+                {
+                    string slovo = Convert.ToString(newText[i]);
+                    if (!dictionary.ContainsKey(slovo))
+                    {
+                        dictionary[slovo] = 1;
+                    }
+                    else dictionary[slovo]++;
+                }
+            }
+
+
+            foreach (var pair in dictionary)
+            {
+                Console.WriteLine(pair.Key + "\t" + pair.Value);
+            }
+            
+
+            Console.ReadKey();
+        }
+    }
+}
